2ad0386e1662672c28354639fe4d0d06dd782f8e
Panachai <panachai.ny@gmail.com>
2022-03-24 18:16:17 +0700

    Update main.go

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+func processA() {
+	time.Sleep(1 * time.Second)
+	fmt.Println("result a")
+}
+
+func processB() {
+	time.Sleep(1 * time.Second)
+	fmt.Println("result b")
+}
+
+func processC() {
+	time.Sleep(1 * time.Second)
+	fmt.Println("result c")
+}
+
+func processD() {
+	time.Sleep(1 * time.Second)
+	fmt.Println("result d")
+}
 
 func main() {
-	fmt.Println("=========")
+	start := time.Now()
+	go processA()
+	go processB()
+	go processC()
+	go processD()
+	fmt.Printf("Use time : %v\n", time.Since(start))
 }
